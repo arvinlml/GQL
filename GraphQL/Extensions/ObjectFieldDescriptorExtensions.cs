2ab7322e6c31d41fd9996276505b6e330985d118
Aravindh Janakiraman <Aravindh.Janakiraman@wu.com>
2021-08-13 14:38:21 -0600

    Complete

--- GraphQL/Extensions/ObjectFieldDescriptorExtensions.cs
+++ GraphQL/Extensions/ObjectFieldDescriptorExtensions.cs
@@ -14,5 +14,22 @@ namespace ConferencePlanner.GraphQL
                 create: s => s.GetRequiredService<IDbContextFactory<TDbContext>>().CreateDbContext(),
                 disposeAsync: (s, c) => c.DisposeAsync());
         }
+
+        public static IObjectFieldDescriptor UseUpperCase(
+            this IObjectFieldDescriptor descriptor)
+        {
+            // TODO : we need a better API for the user.
+            descriptor.Extend().Definition.ResultConverters.Add(
+                new((context, result) =>
+                {
+                    if (result is string s)
+                    {
+                        return s.ToUpperInvariant();
+                    }
+                    return result;
+                }));
+
+            return descriptor;
+        }
     }
 }
